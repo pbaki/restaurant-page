43005cf4f65de5dca79ac914bcf655e579011806
pbaki <pbaki999@gmail.com>
2022-12-21 21:10:04 +0100

    Finished boilerplate code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("qweqwe");
+import './style.css';
+
+ function component() {
+   const element = document.createElement("div");
+
+   element.innerHTML = "aaaaaa";
+
+   return element;
+ }
+ 
+document.body.appendChild(component())
+
+
+
+
+
+console.log("tyrtyrtyrty");
--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,18 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
+  devtool: 'inline-source-map',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
 };
